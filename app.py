aa7ba8722be395bba853798cd53e200d1f8189e1
KitK <147215922+Kitk00@users.noreply.github.com>
2023-11-19 22:13:21 -0500

    new home

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ from ai_images import get_images
 
 
 food_dict = {}
+url_dict = {}
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/start")
 def start():
